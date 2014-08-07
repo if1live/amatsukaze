3a3e2fd129de7c71722da37414db070b01554ae1
libsora.so <libsora25@gmail.com>
2014-08-07 21:19:09 +0900

    fixed screen size for desktop

--- desktop/src/moe/shipduck/amatsukaze/desktop/DesktopLauncher.java
+++ desktop/src/moe/shipduck/amatsukaze/desktop/DesktopLauncher.java
@@ -7,6 +7,14 @@ import moe.shipduck.amatsukaze.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		int width = 1080;
+		int height = 1920;
+		float scale = 0.5f;
+		
+		config.width = (int)(width * scale);
+		config.height = (int)(height * scale);
+		config.title = "Amatsukaze";
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
