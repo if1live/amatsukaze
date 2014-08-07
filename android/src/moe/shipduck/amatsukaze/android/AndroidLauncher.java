932b029ef6b12a8214ae5551c4edfd5a13940808
libsora.so <libsora25@gmail.com>
2014-08-07 21:10:10 +0900

    libgdx base

---
+++ android/src/moe/shipduck/amatsukaze/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package moe.shipduck.amatsukaze.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import moe.shipduck.amatsukaze.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ desktop/src/moe/shipduck/amatsukaze/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package moe.shipduck.amatsukaze.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import moe.shipduck.amatsukaze.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
