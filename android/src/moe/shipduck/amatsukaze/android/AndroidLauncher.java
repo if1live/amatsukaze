fd953f84e46064b89001021cf4f4d9a3077016b7
libsora.so <libsora25@gmail.com>
2014-08-16 15:58:39 +0900

    jogl 관련 삭제. 이거 없이 개발하는게 목표니까

--- android/src/moe/shipduck/amatsukaze/android/AndroidLauncher.java
+++ android/src/moe/shipduck/amatsukaze/android/AndroidLauncher.java
@@ -2,8 +2,10 @@ package moe.shipduck.amatsukaze.android;
 
 import android.os.Bundle;
 
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.tests.g3d.LightsTest;
 
 import moe.shipduck.amatsukaze.MikuMikuGame;
 import moe.shipduck.amatsukaze.MyGdxGame;
@@ -14,6 +16,7 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		//initialize(new MyGdxGame(), config);
-		initialize(new MikuMikuGame(), config);
+		//initialize(new MikuMikuGame(), config);
+		initialize(new LightsTest(), config);
 	}
 }
